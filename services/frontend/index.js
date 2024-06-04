4a30701341afbc5ee261784c3afbad9351254ca9
Damian Velazquez Cafaro <damian.velazquez@aifinyo.de>
2024-06-04 19:59:09 +0200

    Added Helm resources

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 // set the port to the environment variable PORT or 3000
 // also set a custom environment variable MY_ENV_VAR
 const port = process.env.PORT || 3000;
-const my_env_var = process.env.MY_ENV_VAR || 3000;
+const my_env_var = process.env.MY_ENV_VAR || 'not_set';
+const my_sec_var = process.env.MY_SECRET_VAR || 'not_set';
 
 
 // Reads the config.json file
@@ -14,7 +15,7 @@ const config = JSON.parse(configData);
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello, world! The value from config is: ${config.value}\n and the port is ${port}. This is and environment variable. ${my_env_var}\n`);
+  res.end(`Hello, world! The value from config is: ${config.value}.\n The port is ${port}.\n This is and environment variable. ${my_env_var}.\n And this is a secret. ${my_sec_var}.\n`);
 });
 
 server.listen(port, () => {
